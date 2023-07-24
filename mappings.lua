0338927ad545aae911aac98d03a59393a1d9b0c2
Gowtham Subramani <subramani.gowtham@gmail.com>
2023-07-24 22:42:23 +0530

    commenting copilot mappings

--- mappings.lua
+++ mappings.lua
@@ -7,14 +7,14 @@ M.general = {
   },
 }
 
-M.copilot = {
-  i = {
-    ["<C-y>"] = {"<C-y>", "Accept suggetion", opts = { nowait = true } },
-    ["<A-[>"] = {"<M-[>", "Previous suggestion", opts = { nowait = true } },
-    ["<A-]>"] = {"<M-]>", "Next suggestion", opts = { nowait = true } },
-    ["<C-x>"] = {"<C-[>", "Dismiss suggestion", opts = { nowait = true } },
-  }
-}
+-- M.copilot = {
+--   i = {
+--     ["<C-y>"] = {"<C-y>", "Accept suggetion", opts = { nowait = true } },
+--     ["<A-[>"] = {"<M-[>", "Previous suggestion", opts = { nowait = true } },
+--     ["<A-]>"] = {"<M-]>", "Next suggestion", opts = { nowait = true } },
+--     ["<C-x>"] = {"<C-[>", "Dismiss suggestion", opts = { nowait = true } },
+--   }
+-- }
 
 -- more keybinds!
 
